0a669ac5df00dfbf8337d0c1f457c6f4457f3c27
t0mmytrigger <wdevils.biz@gmail.com>
2023-03-24 14:27:19 +0500

    8 lesson

---
+++ lesson8/app/database.py
@@ -0,0 +1,26 @@
+import sqlite3 as sq
+
+
+async def db_start():
+    global db, cur
+    db = sq.connect('tg.db')
+    cur = db.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS accounts("
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                "tg_id INTEGER, "
+                "cart_id TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS items("
+                "i_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                "name TEXT,"
+                "desc TEXT, "
+                "price TEXT, "
+                "photo TEXT, "
+                "brand TEXT)")
+    db.commit()
+
+
+async def cmd_start_db(user_id):
+    user = cur.execute("SELECT * FROM accounts WHERE tg_id == {key}".format(key=user_id)).fetchone()
+    if not user:
+        cur.execute("INSERT INTO accounts (tg_id) VALUES ({key})".format(key=user_id))
+        db.commit()
